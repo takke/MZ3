ba7aa57097788683843fb4b91d11975a337c3c3a
takke <takke@875e8a4c-fb2d-0410-984e-92a9dd373959>
2007-07-05 14:53:04 +0000

    v0.8.1.0b7
    git-svn-id: http://www.mz3.jp/svn/mz3/trunk@100 875e8a4c-fb2d-0410-984e-92a9dd373959

--- src/MZ3/constants.h
+++ src/MZ3/constants.h
@@ -4,7 +4,7 @@
 #pragma once
 
 /// バージョン文字列
-#define MZ3_VERSION_TEXT			L"MZ3.i Version 0.8.1.0 Beta6"
+#define MZ3_VERSION_TEXT			L"MZ3.i Version 0.8.1.0 Beta7"
 
 /// リビジョン番号
 #define MZ3_SVN_REVISION			L"$Rev$" 
