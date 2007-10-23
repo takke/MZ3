8a3f5de4427ef775076550decb3bebf38e144d98
takke <takke@875e8a4c-fb2d-0410-984e-92a9dd373959>
2007-10-23 12:52:22 +0000

    Revを付けた
    git-svn-id: http://www.mz3.jp/svn/mz3/trunk@328 875e8a4c-fb2d-0410-984e-92a9dd373959

--- src/MZ3/version.h
+++ src/MZ3/version.h
@@ -4,7 +4,7 @@
 #define MZ3_VERSION_TEXT			L"Version 0.8.2.0 Beta30" 
 
 /// リビジョン番号
-#define MZ3_SVN_REVISION			L"$Rev: 310 $"
+#define MZ3_SVN_REVISION			L"$Rev$"
 
 /// MZ3 用ユーティリティ
 namespace util
@@ -17,7 +17,7 @@ namespace util
  */
 inline CString GetSourceRevision()
 {
-	CString rev = MZ3_SVN_REVISION;	// $Rev: xx $
+	CString rev = MZ3_SVN_REVISION;	// $Rev$
 	rev.Replace( L"$Rev: ", L" [" );
 	rev.Replace( L" $", L"]" );
 	return rev;
