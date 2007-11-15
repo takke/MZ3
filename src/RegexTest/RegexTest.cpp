8f031f6626fd8a90d10ce4bded5a4dc6b31077de
takke <takke@875e8a4c-fb2d-0410-984e-92a9dd373959>
2007-11-15 11:03:23 +0000

    簡易XMLパーサ用テストコード追加
    git-svn-id: http://www.mz3.jp/svn/mz3/trunk@422 875e8a4c-fb2d-0410-984e-92a9dd373959

--- src/RegexTest/RegexTest.cpp
+++ src/RegexTest/RegexTest.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include "../MZ3/MyRegex.h"
+#include "../MZ3/xml2stl.h"
 
 int test_date()
 {
@@ -67,9 +68,43 @@ int test_date()
 	return 1;
 }
 
+using namespace xml2stl;
+
+int test_xml2stl()
+{
+	printf( "--start--\n" );
+//	getchar();
+	{
+		Container root;
+
+		xml2stl::SimpleXmlParser::loadFromFile( root, L"C:\\Users\\takke\\Desktop\\MZ4.0.9.0.0bXX\\log\\tracks.xml" );
+
+		// dump
+	//	dump_container( root, stdout );
+
+		try {
+			const Node& feed = root.getNode( _T("feed") );
+			size_t n = feed.getChildrenCount();
+			for( size_t i=0; i<n; i++ ) {
+				wprintf( _T("  entry[%d] = %s\n"), 
+					i, 
+					feed.getNode( _T("entry"), i ).getNode( _T("title") ).getText().c_str() );
+			}
+		} catch( NodeNotFoundException& ) {
+			wprintf( _T("node not found...\n") );
+		}
+//		getchar();
+		wprintf( _T("--end--\n") );
+	}
+//	getchar();
+	return 0;
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	setlocale( LC_ALL, "Japanese" ); 
+
+	return test_xml2stl();
 
 //	test_date();
 
