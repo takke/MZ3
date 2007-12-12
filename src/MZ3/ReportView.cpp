856046ad2aa5441d7b1e431ccca9f12dba8be838
takke <takke@875e8a4c-fb2d-0410-984e-92a9dd373959>
2007-12-12 12:33:33 +0000

    NULL参照回避
    git-svn-id: http://www.mz3.jp/svn/mz3/trunk@566 875e8a4c-fb2d-0410-984e-92a9dd373959

--- src/MZ3/ReportView.cpp
+++ src/MZ3/ReportView.cpp
@@ -376,8 +376,12 @@ void CReportView::OnSize(UINT nType, int cx, int cy)
 
 	// スクロールタイプが「ページ単位」なら再計算
 	if( theApp.m_optionMng.m_reportScrollType == option::Option::REPORT_SCROLL_TYPE_PAGE ) {
-		int charHeightOffset = m_detailView->GetCharHeightOffset();
-		m_scrollLine = (hReport / (fontHeight+charHeightOffset)) - 2;
+		if (m_detailView != NULL) {
+			int charHeightOffset = m_detailView->GetCharHeightOffset();
+			if ((fontHeight+charHeightOffset)>0) {
+				m_scrollLine = (hReport / (fontHeight+charHeightOffset)) - 2;
+			}
+		}
 		TRACE(_T("Scrol Line = %d\n"), m_scrollLine);
 	}else{
 		m_scrollLine = theApp.m_optionMng.m_reportScrollLine;
