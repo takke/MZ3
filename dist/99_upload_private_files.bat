55be64f2b2b3364ff9f9ca2aa7a729257fb08549
takke <takke@875e8a4c-fb2d-0410-984e-92a9dd373959>
2007-12-24 05:50:54 +0000

    private ビルドファイル転送スクリプト作成
    git-svn-id: http://www.mz3.jp/svn/mz3/trunk@607 875e8a4c-fb2d-0410-984e-92a9dd373959

---
+++ dist/99_upload_private_files.bat
@@ -0,0 +1,19 @@
+: ビルド用環境変数の設定
+@call 00_set_env.bat
+@set DAT_FILE=99_upload_private_files.dat
+
+: スクリプトファイルの生成
+@echo cd /var/www/mz3/private   > %DAT_FILE%
+@echo put MZ3.%MZ3_VERSION%.CAB >> %DAT_FILE%
+@echo put MZ4.%MZ3_VERSION%.lzh >> %DAT_FILE%
+@echo bye                       >> %DAT_FILE%
+
+: スクリプト確認
+type %DAT_FILE%
+
+: sftp 実行
+type %DAT_FILE% | sftp takeuchi@mz3.jp
+
+: スクリプトファイル削除
+del %DAT_FILE%
+pause
