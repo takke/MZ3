410998bd2667247946e0981ce0599d6fa37ca18e
takke <takke@875e8a4c-fb2d-0410-984e-92a9dd373959>
2008-01-06 07:10:30 +0000

    v0.9.1.0 Beta5
    git-svn-id: http://www.mz3.jp/svn/mz3/trunk@633 875e8a4c-fb2d-0410-984e-92a9dd373959

--- dist/99_upload_private_files.bat
+++ dist/99_upload_private_files.bat
@@ -3,10 +3,16 @@
 @set DAT_FILE=99_upload_private_files.dat
 
 : スクリプトファイルの生成
-@echo cd /var/www/mz3/private   > %DAT_FILE%
-@echo put MZ3.%MZ3_VERSION%.CAB >> %DAT_FILE%
-@echo put MZ4.%MZ3_VERSION%.lzh >> %DAT_FILE%
-@echo bye                       >> %DAT_FILE%
+@echo cd /var/www/mz3/private            > %DAT_FILE%
+@echo put MZ3.%MZ3_VERSION%.CAB x.CAB    >> %DAT_FILE%
+@echo rm MZ3.%MZ3_VERSION%.CAB           >> %DAT_FILE%
+@echo rename x.CAB MZ3.%MZ3_VERSION%.CAB >> %DAT_FILE%
+@echo chmod 744 MZ3.%MZ3_VERSION%.CAB    >> %DAT_FILE%
+@echo put MZ4.%MZ3_VERSION%.lzh x.lzh    >> %DAT_FILE%
+@echo rm MZ4.%MZ3_VERSION%.lzh           >> %DAT_FILE%
+@echo rename x.lzh MZ4.%MZ3_VERSION%.lzh >> %DAT_FILE%
+@echo chmod 744 MZ4.%MZ3_VERSION%.lzh    >> %DAT_FILE%
+@echo bye                                >> %DAT_FILE%
 
 : スクリプト確認
 type %DAT_FILE%
