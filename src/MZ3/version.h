ae1bfa774458501a34bdf7f7fb10b1b8345517b1
takke <takke@875e8a4c-fb2d-0410-984e-92a9dd373959>
2008-04-20 05:11:21 +0000

    v0.9.1.0 Beta15
    git-svn-id: http://www.mz3.jp/svn/mz3/trunk@747 875e8a4c-fb2d-0410-984e-92a9dd373959

--- src/MZ3/version.h
+++ src/MZ3/version.h
@@ -7,7 +7,7 @@
 #pragma once
 
 /// バージョン文字列
-#define MZ3_VERSION_TEXT_SHORT		L"0.9.1.0 Beta14"
+#define MZ3_VERSION_TEXT_SHORT		L"0.9.1.0 Beta15"
 #define MZ3_VERSION_TEXT			L"Version " MZ3_VERSION_TEXT_SHORT
 
 /// リビジョン番号
