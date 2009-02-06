7b281cf658a3e298b1ab78fb7915ccbb1228538f
takke <takke@875e8a4c-fb2d-0410-984e-92a9dd373959>
2009-02-06 11:27:06 +0000

    著作権表示追加
    git-svn-id: http://www.mz3.jp/svn/mz3/trunk@1139 875e8a4c-fb2d-0410-984e-92a9dd373959

--- src/MZ3/script/mz3.lua
+++ src/MZ3/script/mz3.lua
@@ -1,5 +1,13 @@
+--[[
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version
+ 2 of the License, or (at your option) any later version.
+]]
 --------------------------------------------------
 -- MZ3 Script 基本ライブラリ
+--
+-- $Id$
 --------------------------------------------------
 mz3.logger_info('mz3.lua 開始');
 
--- src/MZ3/script/test.lua
+++ src/MZ3/script/test.lua
@@ -1,8 +1,12 @@
-module("test", package.seeall)
-
 --[[
-function fuga()
-	print "fuga"
-	mz3.hoge();
-end
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version
+ 2 of the License, or (at your option) any later version.
 ]]
+--------------------------------------------------
+-- MZ3 Script test
+--
+-- $Id$
+--------------------------------------------------
+module("test", package.seeall)
