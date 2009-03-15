3774200f719299093c4904b33bb4eed4c30006b9
takke <takke@875e8a4c-fb2d-0410-984e-92a9dd373959>
2009-03-15 15:24:41 +0000

    git-svn-id: http://www.mz3.jp/svn/mz3/trunk@1204 875e8a4c-fb2d-0410-984e-92a9dd373959

--- src/MZ3/MZ3.cpp
+++ src/MZ3/MZ3.cpp
@@ -1494,9 +1494,10 @@ bool CMZ3App::MyLuaErrorReport(int status)
 		const char *msg = lua_tostring(L, -1);
 		if (msg == NULL) msg = "(error object is not a string)";
 		CString user_msg;
+		std::wstring msgw = util::my_mbstowcs(msg);
 		user_msg.Format(L"スクリプト実行中にエラーが発生しました。\r\n"
 						L"\r\n"
-						L"%s", CString(msg));
+						L"%s", msgw.c_str());
 		MZ3LOGGER_ERROR(user_msg);
 		::MessageBox(NULL, user_msg, NULL, MB_OK);
 		lua_pop(L, 1);
