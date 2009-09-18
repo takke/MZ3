5db00c909e8ca805c727eab431354be354669fe3
takke <takke@875e8a4c-fb2d-0410-984e-92a9dd373959>
2009-09-18 13:53:14 +0000

    v1.0.8
    git-svn-id: http://www.mz3.jp/svn/mz3/trunk@1356 875e8a4c-fb2d-0410-984e-92a9dd373959

--- src/MZ3/mz3_revision_in.h
+++ src/MZ3/mz3_revision_in.h
@@ -1,1 +1,1 @@
-#define MZ3_SVN_REVISION L"r1347"
+#define MZ3_SVN_REVISION L"r1354"
--- src/MZ3/version.h
+++ src/MZ3/version.h
@@ -7,5 +7,5 @@
 #pragma once
 
 /// ƒo[ƒWƒ‡ƒ“•¶š—ñ
-#define MZ3_VERSION_TEXT_SHORT		L"1.0.7"
+#define MZ3_VERSION_TEXT_SHORT		L"1.0.8"
 #define MZ3_VERSION_TEXT			L"Version " MZ3_VERSION_TEXT_SHORT
