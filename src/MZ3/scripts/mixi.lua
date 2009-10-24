16842cc02047bcc135642db57460f8eb8ecbf025
omaemona <omaemona@875e8a4c-fb2d-0410-984e-92a9dd373959>
2009-10-24 15:05:49 +0000

    ライセンスをGPLから修正BSDライセンスに変更
    git-svn-id: http://www.mz3.jp/svn/mz3/trunk@1385 875e8a4c-fb2d-0410-984e-92a9dd373959

--- src/MZ3/scripts/mixi.lua
+++ src/MZ3/scripts/mixi.lua
@@ -1,8 +1,7 @@
 --[[
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version
- 2 of the License, or (at your option) any later version.
+ * Copyright (c) MZ3 Project Team All rights reserved.
+ * Code licensed under the BSD License:
+ * http://www.mz3.jp/license.txt
 ]]
 --------------------------------------------------
 -- MZ3 Script : mixi
