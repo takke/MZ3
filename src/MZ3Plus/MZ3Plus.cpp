11da14bc6c7af237561dc10211ef1fc6134fd770
takke <takke@875e8a4c-fb2d-0410-984e-92a9dd373959>
2009-11-22 10:13:28 +0000

    mz3plus 追加
    git-svn-id: http://www.mz3.jp/svn/mz3/trunk@1437 875e8a4c-fb2d-0410-984e-92a9dd373959

---
+++ src/MZ3Plus/MZ3Plus.cpp
@@ -0,0 +1,14 @@
+// MZ3Plus.cpp : DLL アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+
+BOOL APIENTRY DllMain( HANDLE hModule, 
+                       DWORD  ul_reason_for_call, 
+                       LPVOID lpReserved
+					 )
+{
+    return TRUE;
+}
+
