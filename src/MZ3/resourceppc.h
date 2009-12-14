a956e9bd19456b83388a926b8a1517e57ac5679e
takke <takke@875e8a4c-fb2d-0410-984e-92a9dd373959>
2009-12-14 14:35:57 +0000

    git-svn-id: http://www.mz3.jp/svn/mz3/trunk@1455 875e8a4c-fb2d-0410-984e-92a9dd373959

--- src/MZ3/resourceppc.h
+++ src/MZ3/resourceppc.h
@@ -338,7 +338,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        180
 #define _APS_NEXT_COMMAND_VALUE         34245
-#define _APS_NEXT_CONTROL_VALUE         1085
+#define _APS_NEXT_CONTROL_VALUE         1086
 #define _APS_NEXT_SYMED_VALUE           118
 #endif
 #endif
