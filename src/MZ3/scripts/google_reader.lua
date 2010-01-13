b4a330fd65dd07853b238b00d660bba48913fc94
takke <takke@875e8a4c-fb2d-0410-984e-92a9dd373959>
2010-01-13 15:55:35 +0000

    Google Reader：スターを付ける機能を追加、詳細画面追加
    git-svn-id: http://www.mz3.jp/svn/mz3/trunk@1545 875e8a4c-fb2d-0410-984e-92a9dd373959

--- src/MZ3/scripts/google_reader.lua
+++ src/MZ3/scripts/google_reader.lua
@@ -29,7 +29,7 @@ mz3.regist_service('GoogleReader', true);
 -- タグリスト
 type = MZ3AccessTypeInfo.create();
 type:set_info_type('category');									-- カテゴリ
-type:set_service_type('GoogleReader');								-- サービス種別
+type:set_service_type('GoogleReader');							-- サービス種別
 type:set_serialize_key('GOOGLE_READER_TAG_LIST');				-- シリアライズキー
 type:set_short_title('GoogleReader タグリスト');				-- 簡易タイトル
 type:set_request_method('GET');									-- リクエストメソッド
@@ -45,7 +45,7 @@ type:set_body_integrated_line_pattern(2, '');
 -- ログイン用
 type = MZ3AccessTypeInfo.create();
 type:set_info_type('category');									-- カテゴリ
-type:set_service_type('GoogleReader');								-- サービス種別
+type:set_service_type('GoogleReader');							-- サービス種別
 type:set_serialize_key('GOOGLE_READER_LOGIN');					-- シリアライズキー
 type:set_short_title('GoogleReader ログイン');					-- 簡易タイトル
 type:set_request_method('POST');								-- リクエストメソッド
@@ -55,7 +55,7 @@ type:set_request_encoding('utf8');								-- エンコーディング
 -- フィード一覧
 type = MZ3AccessTypeInfo.create();
 type:set_info_type('category');									-- カテゴリ
-type:set_service_type('GoogleReader');								-- サービス種別
+type:set_service_type('GoogleReader');							-- サービス種別
 type:set_serialize_key('GOOGLE_READER_ATOM_LIST');				-- シリアライズキー
 type:set_short_title('GoogleReader フィード');					-- 簡易タイトル
 type:set_request_method('GET');									-- リクエストメソッド
@@ -70,10 +70,28 @@ type:set_body_integrated_line_pattern(2, '%2');
 -- フィード
 type = MZ3AccessTypeInfo.create();
 type:set_info_type('other');									-- カテゴリ
-type:set_service_type('GoogleReader');								-- サービス種別
+type:set_service_type('GoogleReader');							-- サービス種別
 type:set_serialize_key('GOOGLE_READER_ATOM_ITEM');				-- シリアライズキー
 type:set_short_title('GoogleReader フィード');					-- 簡易タイトル
 type:set_request_method('GET');									-- リクエストメソッド
+type:set_request_encoding('utf8');								-- エンコーディング
+
+-- トークン
+type = MZ3AccessTypeInfo.create();
+type:set_info_type('other');									-- カテゴリ
+type:set_service_type('GoogleReader');							-- サービス種別
+type:set_serialize_key('GOOGLE_READER_TOKEN');					-- シリアライズキー
+type:set_short_title('GoogleReader Token');						-- 簡易タイトル
+type:set_request_method('GET');									-- リクエストメソッド
+type:set_request_encoding('utf8');								-- エンコーディング
+
+-- スターを付ける
+type = MZ3AccessTypeInfo:create();
+type:set_info_type('post');										-- カテゴリ
+type:set_service_type('GoogleReader');							-- サービス種別
+type:set_serialize_key('GOOGLE_READER_ADD_STAR');				-- シリアライズキー
+type:set_short_title('GoogleReader スター');							-- 簡易タイトル
+type:set_request_method('POST');								-- リクエストメソッド
 type:set_request_encoding('utf8');								-- エンコーディング
 
 
@@ -86,6 +104,7 @@ menu_items = {}
 menu_items.read               = mz3_menu.regist_menu("greader.on_read_menu_item");
 menu_items.open_by_browser    = mz3_menu.regist_menu("greader.on_open_by_browser_menu_item");
 menu_items.add_star           = mz3_menu.regist_menu("greader.on_add_star_menu_item");
+menu_items.show_main_view     = mz3_menu.regist_menu("greader.on_show_main_view");
 
 
 ----------------------------------------
@@ -351,13 +370,13 @@ function google_reader_atom_list_parser(parent, body, html)
 
 		-- id
 		-- <id gr:original-id="http://rss.rssad.jp/rss/ad/xxx/xxx?type=2&ent=xxx">tag:google.com,2005:reader/item/xxx</id>
-		local id = entry_tag:match('<id .->(.-)</');
-		local updated = entry_tag:match('<updated>(.-)</');
-		local title = entry_tag:match('<title.->(.-)</');
+		id = entry_tag:match('<id .->(.-)</');
+		updated = entry_tag:match('<updated>(.-)</');
+		title = entry_tag:match('<title.->(.-)</');
 		title = title:gsub('&amp;', '&');
 		title = mz3.decode_html_entity(title);
-		local author = '';
-		local source = entry_tag:match('<source.->(.-)</source>');
+		author = '';
+		source = entry_tag:match('<source.->(.-)</source>');
 		if source ~= nil then
 			author = source:match('<title.->(.-)</');
 			author = author:gsub('&amp;', '&');
@@ -368,13 +387,13 @@ function google_reader_atom_list_parser(parent, body, html)
 
 		-- URL : <link rel="alternate" href="..."> を対象とする。
 		-- いわゆる代替URLなのでWebページとなる。
-		local url = entry_tag:match('<link rel="alternate" href="(.-)"');
+		url = entry_tag:match('<link rel="alternate" href="(.-)"');
 		if url ~= nil then
 			url = url:gsub('&amp;', '&');
 			-- TODO decode_html_entity 必要？
 		end
 		
-		local summary = entry_tag:match('<summary.->(.-)</summary>');
+		summary = entry_tag:match('<summary.->(.-)</summary>');
 		if summary ~= nil then
 			summary = summary:gsub('&amp;', '&');
 			summary = mz3.decode_html_entity(summary);
@@ -451,6 +470,16 @@ end
 mz3.add_event_listener("get_view_style", "greader.on_get_view_style");
 
 
+--- メイン画面に戻る
+function on_show_main_view(serialize_key, event_name, data)
+	mz3.logger_debug('on_show_main_view : (' .. serialize_key .. ', ' .. event_name .. ')');
+
+	mz3.change_view('main_view');
+
+	return true;
+end
+
+
 --- 全文表示メニューまたはダブルクリックイベント
 function on_read_menu_item(serialize_key, event_name, data)
 	-- メニュー直接の場合はシリアライズキーがないため data から取得する
@@ -470,7 +499,12 @@ function on_read_menu_item(serialize_key, event_name, data)
 	
 	if serialize_key == 'GOOGLE_READER_ATOM_ITEM' then
 		-- フィード詳細
-		-- TODO 本来はTwitter詳細画面かレポートビューで表示すべき
+
+		-- 詳細画面で表示
+		data = mz3_main_view.get_selected_body_item();
+		mz3.show_detail_view(data);
+		return true;
+		--[[
 		item = '';
 		item = item .. data:get_text('name') .. " - " .. data:get_date() .. "\r\n";
 		item = item .. data:get_text('title') .. "\r\n";
@@ -481,6 +515,7 @@ function on_read_menu_item(serialize_key, event_name, data)
 		item = item .. "----\r\n";
 		item = item .. data:get_text('url') .. "\r\n";
 		mz3.alert(item, data:get_text('name'));
+		]]
 	end
 
 	return true;
@@ -512,33 +547,44 @@ end
 
 
 --- スターを付ける
--- TODO
 function on_add_star_menu_item(serialize_key, event_name, data)
 	mz3.logger_debug('on_add_star_menu_item: (' .. serialize_key .. ', ' .. event_name .. ')');
 
-	mz3.alert('本バージョンでは未対応です');
-
-	return true;
---[[
+	-- token 取得
+	url = 'http://www.google.com/reader/api/0/token';
+	referer = '';
+	user_agent = nil;
+	is_blocking = true;
+	access_type = mz3.get_access_type_by_key("GOOGLE_READER_TOKEN");
+	status, token = mz3.open_url(mz3_main_view.get_wnd(), access_type, url, referer, "text", user_agent, nil, is_blocking);
+--	local s = 'status : ' .. status .. '\r\n' .. 'token : [' .. token .. ']';
+--	mz3.alert(s);
+	
+	if status ~= 200 then
+		mz3.alert('トークンの取得に失敗しました。');
+		return true;
+	end
+	
+	-- POST
 	data = MZ3Data:create(data);
 
 	-- POSTパラメータ生成
 	post = MZ3PostData:create();
 
-	-- tパラメータはメールURLから取得する
-	t = data:get_text('url');
-	t = t:match('th=(.*)$');
-	post_body = 'redir=%3F&tact=st&nvp_tbu_go=%E5%AE%9F%E8%A1%8C&t=' .. t .. '&bact=';
-	post:append_post_body(post_body);
+	-- iパラメータはデータから取得する
+	post:append_post_body('i=' .. mz3.url_encode(data:get_text('id'), 'utf8'));
+	post:append_post_body('&a=' .. mz3.url_encode('user/-/state/com.google/starred', 'utf8'));
+	post:append_post_body('&ac=edit');
+	post:append_post_body('&T=' .. token);
+--	mz3.alert(data:get_text('id'));
 
 	-- 通信開始
-	url = data:get_text('post_url');
-	access_type = mz3.get_access_type_by_key("GMAIL_ADD_STAR");
+	url = 'http://www.google.co.jp/reader/api/0/edit-tag?client=MZ3';
+	access_type = mz3.get_access_type_by_key("GOOGLE_READER_ADD_STAR");
 	referer = '';
 	user_agent = nil;
 	mz3.open_url(mz3_main_view.get_wnd(), access_type, url, referer, "text", user_agent, post.post_data);
 	return true;
-]]
 end
 
 
@@ -550,12 +596,10 @@ end
 -- @param filename      レスポンスファイル
 -- @param wnd           wnd
 --
--- TODO OK?
---[[
 function on_post_end(event_name, serialize_key, http_status, filename)
 
 	service_type = mz3.get_service_type(serialize_key);
-	if service_type~="gmail" then
+	if service_type~="GoogleReader" then
 		return false;
 	end
 
@@ -569,20 +613,13 @@ function on_post_end(event_name, serialize_key, http_status, filename)
 	end
 
 	-- リクエストの種別に応じてメッセージを表示
-	if serialize_key == "GMAIL_ADD_STAR" then
+	if serialize_key == "GOOGLE_READER_ADD_STAR" then
 		mz3_main_view.set_info_text("スターつけた！");
-	end
-	if serialize_key == "GMAIL_REMOVE_STAR" then
-		mz3_main_view.set_info_text("スターとったどー！");
-	end
-	if serialize_key == "GMAIL_ARCHIVE" then
-		mz3_main_view.set_info_text("アーカイブしたー！");
 	end
 
 	return true;
 end
-mz3.add_event_listener("post_end", "gmail.on_post_end");
-]]
+mz3.add_event_listener("post_end", "greader.on_post_end");
 
 
 --- ボディリストのダブルクリック(またはEnter)のイベントハンドラ
@@ -647,7 +684,12 @@ function on_popup_body_menu(event_name, serialize_key, body, wnd)
 		-- メニュー生成
 		menu = MZ3Menu:create_popup_menu();
 		
-		menu:append_menu("string", "詳細...", menu_items.read);
+		if event_name == 'popup_body_menu' then
+			menu:append_menu("string", "詳細", menu_items.read);
+		else
+			-- 詳細画面の場合
+			menu:append_menu("string", "メイン画面に戻る", menu_items.show_main_view);
+		end
 		menu:append_menu("string", "ブラウザで開く...", menu_items.open_by_browser);
 		menu:append_menu("separator");
 		menu:append_menu("string", "スターを付ける...", menu_items.add_star);
@@ -666,5 +708,292 @@ function on_popup_body_menu(event_name, serialize_key, body, wnd)
 end
 mz3.add_event_listener("popup_body_menu",  "greader.on_popup_body_menu");
 
+
+--- 詳細画面の描画
+function on_draw_detail_view(event_name, serialize_key, data, dc, cx, cy)
+
+	service_type = mz3.get_service_type(serialize_key);
+	if service_type ~= 'GoogleReader' then
+		return false;
+	end
+
+	-- オブジェクト化
+	data = MZ3Data:create(data);
+	g = MZ3Graphics:create(dc);
+
+	-- 画面端のマージン
+	x_margin = 5;
+	y_margin = 5;
+
+	----------------------------------------------
+	-- ページ番号
+	----------------------------------------------
+	g:set_font_size(-1);		-- 小サイズフォント
+	line_height = g:get_line_height();
+	x = x_margin;
+	y = y_margin;
+
+	-- 項目番号(ページ番号風で)
+	local list = MZ3DataList:create(mz3_main_view.get_body_item_list());
+	local n = list:get_count();
+	idx = mz3_main_view.get_selected_body_item_idx();
+	text = (idx+1) .. '/' .. n;
+	g:set_color("text", "MainBodyListDefaultText");
+	x = x_margin;
+	y = y_margin;
+	w = cx - x - x_margin;
+	h = line_height;
+	format = DT_NOPREFIX + DT_RIGHT;
+	g:draw_text(text, x, y, w, h, format);
+
+
+	----------------------------------------------
+	-- 丈夫の文字列の描画
+	----------------------------------------------
+	g:set_font_size(1);		-- 大サイズフォント
+	line_height = g:get_line_height();
+	
+	-- 名前
+	x = x +x_margin;
+	y = y_margin;
+	w = cx - x - x_margin;
+	h = line_height;
+	format = DT_LEFT;
+	
+	color_text_org = g:set_color("text", "MainBodyListNonreadText");
+	color_bk_org = g:set_color("bk", "MainStatusBG");
+	text = data:get_text('name');
+	g:draw_text(text, x, y, w, h, format);
+
+	-- タイトル
+	g:set_color("text", "MainBodyListDefaultText");
+	g:set_font_size(1);		-- 大サイズフォント
+	y = y + line_height *1.2;
+	text = data:get_text('title') .. "\r\n";
+	g:draw_text(text, x, y, w, h, format);
+
+	-- 日付
+	g:set_color("text", "MainBodyListDefaultText");
+	text = data:get_date();
+	y = y + line_height *1.2;
+	format = DT_RIGHT;
+	g:set_font_size(0);		-- 中サイズフォント
+	line_height = g:get_line_height();
+	g:draw_text(text, x, y, w, h, format);
+
+
+	----------------------------------------------
+	-- 本文
+	----------------------------------------------
+	
+	-- 開始Y座標はクライアント名の下側とアイコンの下側の下の方とする
+	local y_source_bottom = y + line_height + y_margin;
+
+	-- 本文
+	g:set_font_size(1);		-- 大サイズフォント
+	line_height = g:get_line_height();
+	g:set_color("text", "MainBodyListNonreadText");
+	text = data:get_text_array_joined_text('body');
+--	text = text:gsub("\r\n", "");
+	text = text:gsub('<img .->', '[画像]');
+	text = text:gsub('<br.-/?>', '\r\n');
+	text = text:gsub("<.->", "");
+	y = y_source_bottom;
+	-- 高さは画面の高さからN行引いた程度
+--	h = cy * 2 / 3;
+	h = cy - line_height * 6;
+	x = x_margin;
+	w = cx - x - x_margin;
+	
+	-- 本文用枠描画
+	g:draw_rect("border", x, y, w, h, "MainBodyListNonreadText");
+	
+	border_margin = 5;
+	x = x +border_margin;
+	y = y +border_margin;
+	w = w -border_margin*2;
+	h = h -border_margin*2;
+	format = DT_WORDBREAK + DT_NOPREFIX + DT_EDITCONTROL + DT_LEFT + DT_END_ELLIPSIS;
+	g:draw_text(text, x, y, w, h, format);
+
+
+	----------------------------------------------
+	-- 前後項目の描画
+	----------------------------------------------
+	local list = MZ3DataList:create(mz3_main_view.get_body_item_list());
+	local n = list:get_count();
+	local idx = mz3_main_view.get_selected_body_item_idx();
+
+	g:set_font_size(-1);	-- 小サイズフォント
+	line_height = g:get_line_height();
+	h = line_height;
+
+	local y_lr_text = cy - line_height - y_margin;
+
+	-- 前項目の描画
+	if idx > 0 then
+		local f = MZ3Data:create(list:get_data(idx-1));
+		format = DT_NOPREFIX + DT_LEFT;
+
+		x = x_margin  + x_margin;
+		w = cx / 2;
+		g:draw_text('≪' .. f:get_text('name'), x, y_lr_text, w, h, format);
+	end
+
+	-- 次項目の描画
+	if idx < n-1 then
+		local f = MZ3Data:create(list:get_data(idx+1));
+		format = DT_NOPREFIX + DT_RIGHT;
+
+		x = cx /2;
+		w = cx /2  - x_margin;
+		g:draw_text(f:get_text('name') .. '≫', x, y_lr_text, w, h, format);
+	end
+
+	-- 色を戻す
+	g:set_color("text", color_text_org);
+	g:set_color("bk", color_bk_org);
+
+	return true;
+end
+mz3.add_event_listener("draw_detail_view",  "greader.on_draw_detail_view");
+
+
+--- 詳細画面のキー押下イベント
+function on_keydown_detail_view(event_name, serialize_key, data, key)
+	mz3.logger_debug('on_keydown_detail_view : (' .. serialize_key .. ', ' .. event_name .. ', ' .. key .. ')');
+
+	service_type = mz3.get_service_type(serialize_key);
+	if service_type~='GoogleReader' then
+		return false;
+	end
+
+	if key == VK_DOWN or key == VK_UP then
+		-- 下キー：次の発言
+		-- 上キー：前の発言
+		local list = MZ3DataList:create(mz3_main_view.get_body_item_list());
+		local n = list:get_count();
+		local idx = mz3_main_view.get_selected_body_item_idx();
+		if key == VK_DOWN then
+			-- 次の項目を表示
+			if idx < n-1 then
+				mz3_main_view.select_body_item(idx+1);
+			else
+				-- 先頭に戻る
+				if n>0 then
+					mz3_main_view.select_body_item(0);
+				end
+			end
+		else
+			-- 前の項目を表示
+			if idx >= 1 then
+				mz3_main_view.select_body_item(idx-1);
+			else
+				-- 末尾に戻る
+				mz3_main_view.select_body_item(n-1);
+			end
+		end
+		data = mz3_main_view.get_selected_body_item();
+		mz3.show_detail_view(data);
+		return true;
+	end
+	
+	if key == VK_RIGHT or key == VK_LEFT then
+		-- 左右キー
+		return true;
+	end
+	
+	if key == VK_RETURN or key == VK_ESCAPE or key == VK_BACK then
+		-- 閉じる
+		mz3.change_view('main_view');
+		return true;
+	end
+	
+	if key == VK_F2 then
+		-- ボディリストのメニューを表示
+		on_popup_body_menu(event_name, serialize_key, data, mz3_main_view.get_wnd());
+		return true;
+	end
+	
+	return true;
+end
+mz3.add_event_listener("keydown_detail_view", "greader.on_keydown_detail_view");
+
+
+--- 詳細画面のクリックイベント
+function on_click_detail_view(event_name, serialize_key, data, x, y, cx, cy)
+
+	service_type = mz3.get_service_type(serialize_key);
+	if service_type~='GoogleReader' then
+		return false;
+	end
+
+	if y > cy/2 then
+		-- 下側1/2であれば項目移動
+		if x < cx/2 then
+			-- 左側：前の項目を表示
+			on_keydown_detail_view("keydown_detail_view", serialize_key, data, VK_UP);
+		else
+			-- 右側：次の項目を表示
+			on_keydown_detail_view("keydown_detail_view", serialize_key, data, VK_DOWN);
+		end
+	else
+		-- 閉じる
+		mz3.change_view('main_view');
+	end
+	
+	return true;
+end
+mz3.add_event_listener("click_detail_view", "greader.on_click_detail_view");
+
+
+--- 詳細画面の右クリックイベント
+function on_rclick_detail_view(event_name, serialize_key, data, x, y)
+
+	service_type = mz3.get_service_type(serialize_key);
+	if service_type~='GoogleReader' then
+		return false;
+	end
+
+	-- ボディリストのメニューを表示
+	on_popup_body_menu(event_name, serialize_key, data, mz3_main_view.get_wnd());
+	
+	return true;
+end
+mz3.add_event_listener("rclick_detail_view", "greader.on_rclick_detail_view");
+
+
+--- 詳細画面のマウスホイールイベント
+function on_mousewheel_detail_view(event_name, serialize_key, data, z_delta, vkey, x, y)
+
+	service_type = mz3.get_service_type(serialize_key);
+	if service_type~='GoogleReader' then
+		return false;
+	end
+
+--	mz3.logger_debug('z_delta : ' .. z_delta);
+
+	if z_delta > 0 then
+		-- 上方向：前の項目を表示
+		on_keydown_detail_view("keydown_detail_view", serialize_key, data, VK_UP);
+	else
+		-- 下方向：次の項目を表示
+		on_keydown_detail_view("keydown_detail_view", serialize_key, data, VK_DOWN);
+	end
+
+	return true;
+end
+mz3.add_event_listener("mousewheel_detail_view", "greader.on_mousewheel_detail_view");
+
+
+--- 詳細画面のポップアップメニュー
+function on_popup_detail_menu(event_name, serialize_key, data, wnd)
+
+	-- ボディリストのメニューを表示
+	on_popup_body_menu(event_name, serialize_key, data, mz3_main_view.get_wnd());
+	
+	return true;
+end
+mz3.add_event_listener("popup_detail_menu", "greader.on_popup_detail_menu");
 
 mz3.logger_debug('google_reader.lua end');
