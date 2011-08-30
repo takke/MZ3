dd1440a76f50492bdd7d3cef716430fde8f6ab82
yutaka <yutaka@875e8a4c-fb2d-0410-984e-92a9dd373959>
2011-08-30 01:45:46 +0000

    コミュニティ系仕様変更対応（2011.08.29 doutor）
    git-svn-id: http://www.mz3.jp/svn/mz3/trunk@1658 875e8a4c-fb2d-0410-984e-92a9dd373959

--- src/MZ3/scripts/mixi/mixi_new_bbs_parser.lua
+++ src/MZ3/scripts/mixi/mixi_new_bbs_parser.lua
@@ -30,18 +30,142 @@ function new_bbs_parser(parent, body, html)
 
 	-- 全消去
 	body:clear();
-	
+
 	local t1 = mz3.get_tick_count();
 	local in_data_region = false;
-	
+
 	local back_data = nil;
 	local next_data = nil;
-	
+
 	-- 行数取得
 	local line_count = html:get_count();
 	for i=140, line_count-1 do
 		line = html:get_at(i);
-		
+
+--		mz3.logger_debug(i .. " : " .. html:get_at(i));
+
+		-- 次へ、前への抽出処理
+		-- 項目発見前にのみ存在する
+		if not in_data_region and back_data==nil and next_data==nil then
+			back_data, next_data = parse_next_back_link(line, "new_bbs.pl");
+		end
+
+		-- 項目探索
+		if line_has_strings(line, "<div", "class", "postData") then
+
+			in_data_region = true;
+
+			-- data 生成
+			data = MZ3Data:create();
+
+			-- 次行取得
+			i = i+1;
+			line = html:get_at(i);
+
+			-- コミュニティ名
+			name = line:match(">([^<]+)(<.*)$");
+			name = mz3.decode_html_entity(name);
+			data:set_text("name", name);
+
+			-- 次行取得
+			i = i+1;
+			line = html:get_at(i);
+
+			-- 見出し
+			title, after = line:match(">([^<]+)(<.*)$");
+			title = mz3.decode_html_entity(title);
+			mz3.logger_debug(after);
+
+			-- アンケート、イベントの場合はタイトルの前にマークを付ける
+			if line_has_strings(line, "iconEvent") then
+				title = "【☆】" .. title;
+			elseif line_has_strings(line, "iconEnquete") then
+				title = "【＠】" .. title;
+			end
+			data:set_text("title", title);
+
+			-- URL 取得
+			url = line:match("href=\"([^\"]+)\"");
+			data:set_text("url", url);
+
+			-- id
+			id = get_param_from_url(url, "id");
+			data:set_integer("id", id);
+
+			-- 次2行取得
+			i = i+2;
+			line = html:get_at(i);
+
+			-- 日付のパース
+			data:parse_date_line(line);
+
+			-- URL に応じてアクセス種別を設定
+			type = mz3.estimate_access_type_by_url(url);
+			data:set_access_type(type);
+
+			-- data 追加
+			body:add(data.data);
+
+			-- data 削除
+			data:delete();
+		end
+
+		if in_data_region and line_has_strings(line, "</ul>") then
+			mz3.logger_debug("★</ul>が見つかったので終了します");
+			break;
+		end
+
+	end
+
+	-- 前、次へリンクの追加
+	if back_data~=nil then
+		-- 先頭に挿入
+		body:insert(0, back_data.data);
+		back_data:delete();
+	end
+	if next_data~=nil then
+		-- 末尾に追加
+		body:add(next_data.data);
+		next_data:delete();
+	end
+	
+	local t2 = mz3.get_tick_count();
+	mz3.logger_debug("new_bbs_parser end; elapsed : " .. (t2-t1) .. "[msec]");
+end
+
+
+--------------------------------------------------
+-- 【トピック自分の最新書き込み一覧】
+-- [list] new_bbs.pl 用パーサ
+--
+-- http://mixi.jp/new_bbs_comment.pl
+--
+-- 引数:
+--   parent: 上ペインの選択オブジェクト(MZ3Data*)
+--   body:   下ペインのオブジェクト群(MZ3DataList*)
+--   html:   HTMLデータ(CHtmlArray*)
+--------------------------------------------------
+function new_bbs_comment_perser(parent, body, html)
+	mz3.logger_debug("new_bbs_comment_parser start");
+
+	-- wrapperクラス化
+	body = MZ3DataList:create(body);
+	html = MZ3HTMLArray:create(html);
+
+	-- 全消去
+	body:clear();
+
+	local t1 = mz3.get_tick_count();
+	local in_data_region = false;
+
+	local back_data = nil;
+	local next_data = nil;
+
+	-- 行数取得
+	local line_count = html:get_count();
+	for i=140, line_count-1 do
+		line = html:get_at(i);
+
 --		mz3.logger_debug(i .. " : " .. html:get_at(i));
 
 		-- 次へ、前への抽出処理
@@ -63,12 +187,12 @@ function new_bbs_parser(parent, body, html)
 
 			-- 日付のパース
 			data:parse_date_line(line);
-			
+
 			-- 次行取得
 			i = i+1;
 			line2 = html:get_at(i);
 --			mz3.trace(i .. " : " .. line2);
-			
+
 			-- 見出し
 			-- <dd><a href="view_bbs.pl?id=20728968&comment_count=3&comm_id=1198460">
 			-- 【チャット】集え！xxx</a> (MZ3 -Mixi for ZERO3-)</dd>
@@ -84,15 +208,15 @@ function new_bbs_parser(parent, body, html)
 			end
 --			mz3.logger_debug(title);
 			data:set_text("title", title);
-			
+
 			-- URL 取得
 			url = line2:match("href=\"([^\"]+)\"");
 --			mz3.logger_debug(url);
 			data:set_text("url", url);
-			
+
 			-- コメント数
 			data:set_integer("comment_count", get_param_from_url(url, "comment_count"));
-			
+
 			-- id
 			id = get_param_from_url(url, "id");
 			data:set_integer("id", id);
@@ -101,14 +225,14 @@ function new_bbs_parser(parent, body, html)
 			name = after:match("</a>[^(]*[(](.*)[)]</dd>");
 			name = mz3.decode_html_entity(name);
 			data:set_text("name", name);
-			
+
 			-- URL に応じてアクセス種別を設定
 			type = mz3.estimate_access_type_by_url(url);
 			data:set_access_type(type);
-			
+
 			-- data 追加
 			body:add(data.data);
-			
+
 			-- data 削除
 			data:delete();
 		end
@@ -119,7 +243,7 @@ function new_bbs_parser(parent, body, html)
 		end
 
 	end
-	
+
 	-- 前、次へリンクの追加
 	if back_data~=nil then
 		-- 先頭に挿入
@@ -131,12 +255,13 @@ function new_bbs_parser(parent, body, html)
 		body:add(next_data.data);
 		next_data:delete();
 	end
-	
+
 	local t2 = mz3.get_tick_count();
-	mz3.logger_debug("new_bbs_parser end; elapsed : " .. (t2-t1) .. "[msec]");
+	mz3.logger_debug("new_bbs_comment_parser end; elapsed : " .. (t2-t1) .. "[msec]");
 end
+
 
 -- コミュニティ最新書込一覧
 mz3.set_parser("BBS",             "mixi.new_bbs_parser");
 -- コミュニティコメント記入履歴 : 最新書込一覧と同一
-mz3.set_parser("NEW_BBS_COMMENT", "mixi.new_bbs_parser");
+mz3.set_parser("NEW_BBS_COMMENT", "mixi.new_bbs_comment_perser");
