c8b2aeac98225ddd27a481eed0e99545bd8bb620
Hiroaki TAKEUCHI <takke30@gmail.com>
2013-06-18 01:30:06 +0900

    リリースファイル名変更

--- dist/99_release.bat
+++ dist/99_release.bat
@@ -1,7 +1,7 @@
 : ビルド用環境変数の設定
 @call 00_set_env.bat
 
-:----- CAB/LZH ファイル名の変更 -----
+:----- CAB/zip ファイル名の変更 -----
 @echo MZ3.%MZ3_VERSION%.CAB 等を別ファイル名に変更できます。
 @set NEW_VER=0
 @set /P NEW_VER="変更する場合は新バージョン番号を入力して下さい(例:1.2.3) ※Enterでキャンセル > "
@@ -17,14 +17,14 @@ IF %NEW_VER%==0 GOTO END
 @echo "MZ3Plus.%MZ3_VERSION%.CAB  => MZ3Plus.%NEW_VER%.CAB"
 @move  MZ3Plus.%MZ3_VERSION%.CAB     MZ3Plus.%NEW_VER%.CAB
 
-@echo "MZ4.%MZ3_VERSION%.lzh      => MZ4.%NEW_VER%.lzh"
-@move  MZ4.%MZ3_VERSION%.lzh         MZ4.%NEW_VER%.lzh
+@echo "MZ4.%MZ3_VERSION%.zip      => MZ4.%NEW_VER%.zip"
+@move  MZ4.%MZ3_VERSION%.zip         MZ4.%NEW_VER%.zip
 
 @echo "TkTweets.%MZ3_VERSION%.CAB => TkTweets.%NEW_VER%.CAB"
 @move  TkTweets.%MZ3_VERSION%.CAB    TkTweets.%NEW_VER%.CAB
 
-@echo "TkTweets.%MZ3_VERSION%.lzh => TkTweets.%NEW_VER%.lzh"
-@move  TkTweets.%MZ3_VERSION%.lzh    TkTweets.%NEW_VER%.lzh
+@echo "TkTweets.%MZ3_VERSION%.zip => TkTweets.%NEW_VER%.zip"
+@move  TkTweets.%MZ3_VERSION%.zip    TkTweets.%NEW_VER%.zip
 
 :----- アップロード -----
 @set DAT_FILE=99_upload_bin_files.dat
@@ -38,20 +38,20 @@ IF %NEW_VER%==0 GOTO END
 @echo rename x.CAB MZ3.%MZ3_VERSION%.CAB      >> %DAT_FILE%
 @echo chmod 744 MZ3.%MZ3_VERSION%.CAB         >> %DAT_FILE%
 
-@echo put MZ4.%MZ3_VERSION%.lzh x.lzh         >> %DAT_FILE%
-@echo rm MZ4.%MZ3_VERSION%.lzh                >> %DAT_FILE%
-@echo rename x.lzh MZ4.%MZ3_VERSION%.lzh      >> %DAT_FILE%
-@echo chmod 744 MZ4.%MZ3_VERSION%.lzh         >> %DAT_FILE%
+@echo put MZ4.%MZ3_VERSION%.zip x.zip         >> %DAT_FILE%
+@echo rm MZ4.%MZ3_VERSION%.zip                >> %DAT_FILE%
+@echo rename x.zip MZ4.%MZ3_VERSION%.zip      >> %DAT_FILE%
+@echo chmod 744 MZ4.%MZ3_VERSION%.zip         >> %DAT_FILE%
 
 @echo put TkTweets.%MZ3_VERSION%.CAB x.CAB    >> %DAT_FILE%
 @echo rm TkTweets.%MZ3_VERSION%.CAB           >> %DAT_FILE%
 @echo rename x.CAB TkTweets.%MZ3_VERSION%.CAB >> %DAT_FILE%
 @echo chmod 744 TkTweets.%MZ3_VERSION%.CAB    >> %DAT_FILE%
 
-@echo put TkTweets.%MZ3_VERSION%.lzh x.lzh    >> %DAT_FILE%
-@echo rm TkTweets.%MZ3_VERSION%.lzh           >> %DAT_FILE%
-@echo rename x.lzh TkTweets.%MZ3_VERSION%.lzh >> %DAT_FILE%
-@echo chmod 744 TkTweets.%MZ3_VERSION%.lzh    >> %DAT_FILE%
+@echo put TkTweets.%MZ3_VERSION%.zip x.zip    >> %DAT_FILE%
+@echo rm TkTweets.%MZ3_VERSION%.zip           >> %DAT_FILE%
+@echo rename x.zip TkTweets.%MZ3_VERSION%.zip >> %DAT_FILE%
+@echo chmod 744 TkTweets.%MZ3_VERSION%.zip    >> %DAT_FILE%
 
 @echo bye                                     >> %DAT_FILE%
 
